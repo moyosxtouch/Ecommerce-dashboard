22c82d0275474de6be7d1aca51d9bc41ce738366
moyo <moyosxtouch@gmail.com>
2024-09-27 15:47:45 -0600

    register

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,7 @@
-import React from "react"
-import {useRoutes} from "react-router-dom"
-const Router=({
-  allRoutes
-}){
-  const routes=useRoutes([...allRoutes])
-  return routes
-}
-export default Router
+import React from "react";
+import { useRoutes } from "react-router-dom";
+const Router = ({ allRoutes }) => {
+  const routes = useRoutes([...allRoutes]);
+  return routes;
+};
+export default Router;
