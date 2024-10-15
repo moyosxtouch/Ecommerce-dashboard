ae85582d7c4ad43c5188ce735727fac672c82e9b
moyo <moyosxtouch@gmail.com>
2024-10-14 18:49:40 -0600

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Router from "./router/Router";
 import publicRoutes from "./router/routes/publicRoutes";
+import { getRoutes } from "./router/routes";
 function App() {
   const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
+  useEffect(() => {
+    const routes = getRoutes();
+    setAllRoutes([...allRoutes, routes]);
+  }, [allRoutes]);
   return <Router allRoutes={allRoutes} />;
 }
 
