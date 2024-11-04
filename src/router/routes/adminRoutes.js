743042ce6ae6210739646188307d4a0b83843404
moyo <moyosxtouch@gmail.com>
2024-11-04 17:21:29 -0600

    begin categoryPage design creation

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,6 +1,8 @@
 import { lazy } from "react";
+
 const AdminDashboard = lazy(() => import("../../views/admin/AdminDashboard"));
 const Orders = lazy(() => import("../../views/admin/Orders"));
+const Category = lazy(() => import("../../views/admin/Category"));
 
 export const adminRoutes = [
   {
@@ -13,4 +15,5 @@ export const adminRoutes = [
     element: <Orders />,
     role: "admin",
   },
+  { path: "admin/dashboard/category", element: <Category />, role: "admin" },
 ];
